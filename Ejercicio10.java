7fcd0394c6b1abd44fe490c978b83761fe7acfd7
jritsqmet <121739152+jritsqmet@users.noreply.github.com>
2025-03-12 20:28:01 -0500

    .

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -1,16 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.itsqmet.ejercicio10;
 
-/**
- *
- * @author tw
- */
+import javax.swing.JOptionPane;
+
 public class Ejercicio10 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /*
+            Crear un programa que permita calcular el área del cuadrado, (A = lado*lado )
+            el área del triángulo (A= (base*altura) /2 ) y el área del circulo ( A = PI* radio*radio)
+            El usuario va a elegir que figura va a calcular
+        */
+        
+        double lado, base, altura, radio, area=0;
+        String figura;
+        
+        //Ingresar Información
+        
+        figura = JOptionPane.showInputDialog("Ingrese la figura a  calcular el área \n Triangulo \n Cuadrado \n Circulo");
+        
+        if( figura.equals("triangulo")){
+            base = Double.parseDouble( JOptionPane.showInputDialog("Ingresar la base") );
+            altura = Double.parseDouble( JOptionPane.showInputDialog("Ingresar la altura") );
+            
+            area= (base * altura)/2;
+        }
+        
+
+        JOptionPane.showMessageDialog(null, "El área del " + figura + " es: " + area);
+        
+        
+        
+        
     }
 }
