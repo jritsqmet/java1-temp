268e1a5324522cd53c552bb9539462d745f8a938
jritsqmet <121739152+jritsqmet@users.noreply.github.com>
2025-03-12 20:11:38 -0500

    primer commit

---
+++ Ejercicio10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itsqmet.ejercicio10;
+
+/**
+ *
+ * @author tw
+ */
+public class Ejercicio10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
